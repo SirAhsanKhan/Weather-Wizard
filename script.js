b8ac921995b7f4ef7c5626c51f47fd1816f68dc7
Ahsan Khan <afzalahsankhan283@gmail.com>
2024-09-07 00:32:23 +0500

    small update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
